0565eedb072fae692788af41ffe8b3a3afe4b43d
泉　麻彩 <izumi@izumiasairodorinoMacBook-Pro.local>
2019-04-10 17:59:32 +0900

    再提出

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,13 +13,6 @@ class User < ApplicationRecord
    validates :username, presence: true
 
    VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-   validates :email, presence: true, uniqueness: true, format: { with: VALID_EMAIL_REGEX }
-
- def email_required?
-   false
- end
- def email_changed?
-   false
- end
+   validates :email, presence: true
 
 end
